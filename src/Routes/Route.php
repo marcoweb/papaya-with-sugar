b04e59c279d8fafae95901b22f44de2bfaebc3d9
Marco Carvalho <marcocarvalho.web@gmail.com>
2022-09-21 17:34:34 -0300

    routes

---
+++ src/Routes/Route.php
@@ -0,0 +1,41 @@
+<?php
+namespace PapayaWithSugar\Routes;
+
+class Route {
+    private string $name = null;
+    private string $controllerName = null;
+    private string $actionName= null;
+    private string $pattern = null;
+
+	function getName() {
+		return $this->name;
+	}
+	
+	function setName(string $name) : void {
+		$this->name = $name;
+	}
+
+    function getControllerName() : string {
+		return $this->controllerName;
+	}
+	
+	function setControllerName(string $controllerName) : void {
+		$this->controllerName = $controllerName;
+	}
+
+    function getActionName() : string {
+		return $this->actionName;
+	}
+	
+	function setActionName(string $actionName) : void {
+		$this->actionName = $actionName;
+	}
+
+    function getPattern() : string {
+		return $this->pattern;
+	}
+	
+	function setPattern(string $pattern) : void {
+		$this->pattern = $pattern;
+	}
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'marcoweb/papaya-with-sugar',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e164b44b58377015e5b6c505b6e89d932f432127',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'marcoweb/papaya-with-sugar' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e164b44b58377015e5b6c505b6e89d932f432127',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
